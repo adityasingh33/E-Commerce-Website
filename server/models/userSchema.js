0be87b302501f434c2cdea8f880e8a4439a05c89
Aditya Singh <adityasinghbharadwaj688@gmail.com>
2025-08-28 22:25:04 +0530

    Made some changes in the destructuring of role

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
     {
@@ -15,6 +16,12 @@ const userSchema = new mongoose.Schema(
             type: String,
             required: [true],
         },
+        role: {
+            type: String,
+            enum: ['buyer', 'seller'],
+            default: 'buyer'
+        },
+      
         sellerInfo: {
             shopName: { type: String },
             gstNumber: { type: String },
@@ -25,4 +32,18 @@ const userSchema = new mongoose.Schema(
     }
 );
 
-export default mongoose.model('User',userSchema);
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+export default mongoose.model('User', userSchema);
