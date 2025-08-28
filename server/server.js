cac77840cba103b403bd32610e234413eb660658
Aditya Singh <adityasinghbharadwaj688@gmail.com>
2025-08-28 23:07:25 +0530

    Verified the user auth in postman

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config();
 
@@ -16,6 +17,8 @@ app.use(express.json());
 app.get('/',(req,res) => {
     res.json({message : "api is running"})
 });
+
+app.use('/api/users',userRoutes);
 
 const PORT  = process.env.PORT || 5000;
 
