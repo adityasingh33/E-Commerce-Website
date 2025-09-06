0a6ab10a55e33c53377e627ca9cde47e30bb0d10
Aditya Singh <adityasinghbharadwaj688@gmail.com>
2025-09-06 23:32:33 +0530

    Added all the frontend component

---
+++ client/src/pages/Auth.jsx
@@ -0,0 +1,13 @@
+import {React,useState }from 'react'
+import authService from '../feature/auth/authService.jsx'
+import { useNavigate } from 'react-router-dom'
+
+const Auth = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Auth
