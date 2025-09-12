aa8fcbaa9931e14983e73dffdd4a5f7b6265d91f
Aditya Singh <adityasinghbharadwaj688@gmail.com>
2025-09-12 16:37:56 +0530

    Fixed the error of the codes and checked all the workings

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,11 +1,13 @@
 import express from "express"
-import { getAllProducts,createProduct,deleteProduct,updateProduct,getProduct} from "../controllers/productController.js"
+import { getSellerProducts,getAllProducts,createProduct,deleteProduct,updateProduct,getProduct} from "../controllers/productController.js"
 import { protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router();
 
 
 router.post('/seller', protect, createProduct);
+
+router.get('/myproducts', protect, getSellerProducts);
 
 router.get('/',  getAllProducts);
 
